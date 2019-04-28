e5f85a850a1221ea4f9c8fadadca93b9d95e0f2c
Yuming <lionpkqq@gmail.com>
2019-04-28 00:30:26 -0700

    ABC

---
+++ Homework 1/Assets/Circular.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circular : MonoBehaviour
+{
+    public float time = 0;
+    private float radius = 2.0f;
+    public GameObject center;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.time += Time.deltaTime;
+        float x = Mathf.Cos(this.time) * this.radius + this.center.transform.position.x;
+        float y = this.center.transform.position.y;
+        float z = Mathf.Sin(this.time) * this.radius + this.center.transform.position.z;
+
+        this.transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ Homework 1/Assets/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), 1);
+    }
+}
